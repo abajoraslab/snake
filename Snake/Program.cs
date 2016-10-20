acca3584718d5e49e501f70e1845ef17fb6e9a87
abajoraslab <info@bajoras.net>
2016-10-21 00:37:21 +0300

    Hello World

---
+++ Snake/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode; //konvertacija i Unicode -gerai veikia !!!
+            Console.InputEncoding = Encoding.Unicode; //konvertacija i Unicode -gerai veikia !!!
+
+            Console.WriteLine("Hello World - Привет Мир");
+            Console.ReadLine();
+        }
+    }
+}
